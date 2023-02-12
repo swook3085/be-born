0720b009ecd13c47af26b397e800af7bfb014c38
승욱 김 <sw.kim@kyobobook.com>
2023-02-12 23:26:25 +0900

    오류 수정

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,7 +1,7 @@
 export const LOCAL_DOMAIN = 'http://localhost:8081'
 export const API_KEY =
   'jxdNXHd7MviV0OG96kcvMLDsmrrUH4VJcJ5gizCALgi1jMmyz5tA4sJ3PCByGqHd381IPz3UCtTCnGuX0dOZgQ%3D%3D'
-export const API_URL = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/'
+export const API_URL = 'https://apis.data.go.kr/1543061/abandonmentPublicSrvc/'
 export const PAGE_LIMIT = 20
 export const IMAGE_BLUR = {
   placeholder: 'blur',
