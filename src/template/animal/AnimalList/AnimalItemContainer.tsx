aec26c032114f8778ed32d8ee85cbd47fba453bb
승욱 김 <sw.kim@kyobobook.com>
2023-03-08 21:21:14 +0900

    오류 수정

--- src/template/animal/AnimalList/AnimalItemContainer.tsx
+++ src/template/animal/AnimalList/AnimalItemContainer.tsx
@@ -1,22 +1,16 @@
 import useDevice from '@shared/hooks/useDevice'
 import { IAnimalListItem } from '@shared/interface/IPet'
-import { LegacyRef, Ref, forwardRef } from 'react'
+import { Ref, forwardRef } from 'react'
 import MobileAnimalItem from './MobileAnimalItem'
 import PcAnimalItem from './PcAnimalItem'
-import { useSelector } from 'react-redux'
-import { ReducerType } from '@modules/store/rootReducer'
-import { IDetailParam } from '@modules/store/slices/detailParam'
 
 const AnimalItem = forwardRef(
   (item: IAnimalListItem, ref: Ref<HTMLDivElement>) => {
     const { isPc } = useDevice()
-    const detail = useSelector<ReducerType, IDetailParam>(
-      (state) => state.sliceDetailParam,
-    )
     return !isPc ? (
-      <MobileAnimalItem ref={ref} item={item} detail={detail} />
+      <MobileAnimalItem ref={ref} item={item} />
     ) : (
-      <PcAnimalItem item={item} detail={detail} />
+      <PcAnimalItem item={item} />
     )
   },
 )
